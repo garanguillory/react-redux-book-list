863c858288f2b9dff628b400fcc2ea14f4e1dc68
Garan Guillory <garanguillory@gmail.com>
2016-08-20 21:06:16 -0500

    setting up basic book list app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+export default function(){
+	return [
+		{id: 1, title: "Darkly Dreaming Dexter", author: "Jeff Lindsay", pages: 288},
+		{id: 2, title: "Dearly Devoted Dexter", author: "Jeff Lindsay", pages: 292},
+		{id: 3, title: "Dexter in the Dark", author: "Jeff Lindsay", pages: 320},
+		{id: 4, title: "Dexter by Design", author: "Jeff Lindsay", pages: 304},
+		{id: 5, title: "Dexter is Delicious", author: "Jeff Lindsay", pages: 384},
+		{id: 6, title: "Double Dexter", author: "Jeff Lindsay", pages: 337},
+		{id: 7, title: "Dexter's Final Dexter", author: "Jeff Lindsay", pages: 368},
+		{id: 8, title: "Dexter is Dead", author: "Jeff Lindsay", pages: 304}
+	]
+}
